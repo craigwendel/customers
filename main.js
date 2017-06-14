efdb554266cde44d7248bf503cb8728d740d3726
Craig Wendel <wendelcraig@gmail.com>
2017-06-14 15:12:18 -0500

    Add JavaScript to pull in data from customer API. Add HTML with JS. Edit style in CSS according to mockup.

---
+++ main.js
@@ -0,0 +1,50 @@
+// 1. Fetch your users data
+// 2. Loop over the data
+// 3. Display it in the `.customers` element
+
+(function () {
+
+  'use strict';
+
+  function fetchPerson(){
+
+  fetch("https://randomuser.me/api/")
+    .then( function(response){
+      return response.json()
+    })
+    .then(function(json){
+      console.log("data", json)
+      const image = json.results[0].picture.large;
+      const firstName = json.results[0].name.first;
+      const lastName = json.results[0].name.last;
+      const email = json.results[0].email;
+      const streetName = json.results[0].location.street;
+      const city = json.results[0].location.city;
+      const state = json.results[0].location.state;
+      const postalCode = json.results[0].location.postcode;
+      const phone = json.results[0].phone;
+
+      const html = `
+        <div class="customers">
+          <div class="customers-info">
+            <div class="image"><img src="${image}"/></div>
+            <div id="full-name">
+               ${firstName} ${lastName}
+            </div>
+            <div class="details" id ="email">${email}</div>
+            <div class="details">${streetName}</div>
+            <div class="details">
+              ${city} ${state} ${postalCode}
+            </div>
+            <div class="details">${phone}</div>
+          </div>
+        </div>`
+
+      document.querySelector(".customers").insertAdjacentHTML('afterbegin', html)
+    })
+}
+for (var i = 1; i <= 12; i++) {
+  fetchPerson(i)
+}
+
+})();
